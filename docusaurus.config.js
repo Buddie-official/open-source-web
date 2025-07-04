129ba37459712c56b3d2f2edfd61ce4ea5482f9d
Nanachilil <24212010050@m.fudan.edu.cn>
2025-07-04 16:24:05 +0800

    修复崩溃bug: config中url不存在

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: "https://github.com/Buddie-official",
+  url: "https://buddie-official.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/open-source-web/",
