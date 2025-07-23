10e7d1deecb1629ec457f0a993d09cae3a04a4f6
Nanachilil <24212010050@m.fudan.edu.cn>
2025-07-23 20:47:35 +0800

    添加中文首页

--- i18n/zh/docusaurus-plugin-content-pages/HomepageFeatures/index.js
+++ i18n/zh/docusaurus-plugin-content-pages/HomepageFeatures/index.js
@@ -1,48 +1,46 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
-import Link from '@docusaurus/Link';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
+import Link from "@docusaurus/Link";
 
 const FeatureList = [
   {
-    title: '完整的移动端配套',
-    image: require('@site/static/img/home_app_picture.png').default,
+    title: "移动端应用",
+    image: require("@site/static/img/home_app_picture.png").default,
     description: (
       <>
-        一套完整的移动端语音交互系统：
-        开源 AI 逻辑、语音模型框架与通信流程，
-        开箱即用，灵活集成到你的智能设备。
+一个面向智能AI语音交互的完整移动端解决方案，包含开源的 AI 交互逻辑、语音模型，以及与耳机固件的数据传输方式。
       </>
     ),
-    link_url: '/',
+    link_url: "/",
   },
   {
-    title: '可自定义的固件系统',
-    image: require('@site/static/img/home_firmware_picture.png').default,
+    title: "嵌入式固件源码",
+    image: require("@site/static/img/home_firmware_picture.png").default,
     description: (
       <>
-        基于杰理芯片的一整套功能完善、可定制的固件，涵盖音频协议、压缩算法与系统控制逻辑，并与 PCB 配置深度匹配，便于快速移植与功能扩展。
+基于杰理芯片的全功能可定制固件。内含音频协议、压缩算法与系统控制模块，固件设计与 PCB 配置无缝对齐，便于适配和扩展。
       </>
     ),
-    link_url: '/',
+    link_url: "/",
   },
   {
-    title: '开放的硬件设计',
-    image: require('@site/static/img/home_pcb_picture.png').default,
+    title: "电路设计图",
+    image: require("@site/static/img/home_pcb_picture.png").default,
     description: (
       <>
-        全面开放 PCB 布局图、原理图与 BOM 表，适用于 DIY 装配、快速原型设计或定制硬件开发。
+完全开放的 PCB 设计。提供电路板布局图、原理图和 BOM 物料清单。适用于 DIY 装配、快速打样或个性化硬件开发。
       </>
     ),
-    link_url: '/',
+    link_url: "/",
   },
 ];
 
-function Feature({image, title, description, link_url}) {
+function Feature({ image, title, description, link_url }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
-        {/* { <Svg className={styles.featureSvg} role="img" /> } */}
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
         <img src={image} className={styles.featureImage} alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
@@ -50,11 +48,8 @@ function Feature({image, title, description, link_url}) {
         <p>{description}</p>
       </div>
       <div className={styles.buttons}>
-        <Link
-          className="github_button"
-          to = {link_url}
-        >
-          查看文档
+        <Link className="github_button" to={link_url}>
+          Goto Docs
         </Link>
       </div>
     </div>
@@ -64,6 +59,17 @@ function Feature({image, title, description, link_url}) {
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
+      <Heading as="h2" className={clsx("hero_othertitle")}>
+      <p className={styles.hero_othertitle}>
+        Buddie 的硬件与软件，现已开源
+      </p>
+      </Heading>
+      <p className={clsx("text__description")}>
+你可以自由访问 Buddie 的完整技术栈，包括耳机的电路设计图、嵌入式固件源码，以及移动端应用的全部代码。
+      </p>
+      <p className={clsx("text__description")}>
+我们希望这些资源能帮助开发者学习、改进，甚至打造属于自己的语音交互产品。
+      </p>
       <div className="container">
         <div className="row">
           {FeatureList.map((props, idx) => (
--- i18n/zh/docusaurus-plugin-content-pages/index.js
+++ i18n/zh/docusaurus-plugin-content-pages/index.js
@@ -9,30 +9,184 @@ import HomepageFeatures from "./HomepageFeatures"; // 你自己定义的首页�
 // 页面头部使用的标题组件和样式
 import Heading from "@theme/Heading";
 import styles from "./index.module.css";
+import "./css/custom.css"; // 自定义样式文件
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
+    <header
+      className={clsx(
+        "hero hero--primary",
+        styles.heroBanner,
+        styles.Headercolor,
+      )}
+    >
+      <div className={clsx("container")}>
         <Heading as="h1" className="hero__title">
-          {siteConfig.customFields.homepageContent.zh.title}
+          {siteConfig.customFields.homepageContent.en.title}
         </Heading>
         <p className="hero__subtitle">
-            Buddie 是一款已正式发布的智能耳机产品。<br />
-            我们全面开放其硬件设计、固件代码与移动端 App。<br />
-            这是一个面向未来的人机语音交互开源项目，支持用户自由学习、修改与拓展。
+          我们致力于打造面向 AGI 时代的智能语音交互系统，提供完整的技术方案、软硬件协同设计。
+        </p>
+        <p className="hero__subtitle">
+作为一个开放的开发者平台，我们不仅推动技术的普及与落地，也鼓励开发者之间的协作、分享与交流，共同探索通用人工智能背景下的语音交互新范式。
         </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="docs/QuickStart/"
           >
-            快速开始
+            Quick Start
           </Link>
         </div>
       </div>
     </header>
+  );
+}
+
+/*--------------------------------------------------*/
+/*---------------- 工作展示 ------------------------*/
+/*--------------------------------------------------*/
+function HomepageWorkDisplay() {
+  return (
+    <div className={styles.workShow}>
+      <div className={styles.workShowTitle}>
+        <Heading as="h2" className={clsx("hero_othertitle")}>
+          打造 AI 语音交互生态圈
+        </Heading>
+      </div>
+      <div className={styles.workCardrow}>
+        <div className={styles.workCard}>
+          <div className={clsx("card__text")}>
+            <cardTitletext className={clsx("card__tile")}>
+              多场景 AI 语音交互硬件解决方案
+            </cardTitletext>
+          </div>
+          <div className={styles.cardDescription}>
+            <cardText className={clsx("card__text")}>
+我们专注于 AI 语音交互领域，打造涵盖智能音箱、儿童护手环等多种形态的智能终端产品。通过软硬件一体化设计，我们致力于提供既智能又安全的用户体验。
+            </cardText>
+            <div className={styles.cardImage}>
+              <img
+                src="img/products.png"
+                alt="Hardware Solution"
+                style={{
+                  height: "100%",
+                  margin: "0 auto",
+                  aspectRatio: "1 / 1",
+                  objectFit: "contain",
+                  display: "block",
+                  borderRadius: "12px",
+                }}
+              ></img>
+            </div>
+          </div>
+        </div>
+        <div className={styles.workCard}>
+          <div className={styles.cardText}>
+            <cardTitletext className={clsx("card__tile")}>
+              融合 MCP 服务能力的多模态智能交互系统       
+            </cardTitletext>
+          </div>
+          <div className={styles.cardDescription}>
+            <cardText className={clsx("card__text")}>
+通过语音识别、内容摘要、MCP 服务与对话模型集成，我们打造了一套可落地的智能交互系统，覆盖从语音输入到语义输出的完整链路，适用于办公协作、客户服务与知识管理等多种场景。
+            </cardText>
+            <div className={styles.cardImage}>
+              <img
+                src="img/MCP.png"
+                alt="Hardware Solution"
+                style={{
+                  height: "100%",
+                  margin: "0 auto",
+                  aspectRatio: "1 / 1",
+                  objectFit: "contain",
+                  display: "block",
+                  borderRadius: "12px",
+                }}
+              ></img>
+            </div>
+          </div>
+         </div>
+        <div className={styles.workCard}>
+          <div className={styles.cardText}>
+            <cardTitletext className={clsx("card__tile")}>
+              自动化固件开发平台，支持跨平台快速部署
+            </cardTitletext>
+          </div>
+          <div className={styles.cardDescription}>
+            <cardText className={clsx("card__text")}>
+我们构建了一套面向嵌入式设备的自动化固件开发平台，支持从代码编辑、构建到多平台部署的一体化流程。通过模块化架构与脚本自动化，用户可快速适配不同芯片与硬件平台，大幅提升开发效率与交付速度。
+            </cardText>
+            <div className={styles.cardImage}>
+              <img
+                src="img/autocode.png"
+                alt="Hardware Solution"
+                style={{
+                  height: "100%",
+                  margin: "0 auto",
+                  aspectRatio: "1 / 1",
+                  objectFit: "contain",
+                  display: "block",
+                  borderRadius: "12px",
+                }}
+              ></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*--------------------------------------------------*/
+/*---------------- 产品展示 ------------------------*/
+/*--------------------------------------------------*/
+function HomepageProductDisplay() {
+  return (
+    <div className={styles.productShow}>
+      <Heading as="h2" className={clsx("hero_othertitle")} style={{ flex: 1 }}>
+        初代硬件产品-BUDDIE AI 语音交互耳机
+      </Heading>
+
+      <div
+        style={{
+          marginTop: "2rem",
+          display: "flex",
+          justifyContent: "center",
+          flex: 1,
+          width: "70%",
+        }}
+      >
+        {/* 包裹容器控制比例 */}
+        <div
+          style={{
+            width: "60%",
+            aspectRatio: "16 / 9",
+            position: "relative",
+            borderRadius: "12px",
+            overflow: "hidden",
+            marigin: "50 auto",
+          }}
+        >
+          <iframe
+            src="https://www.youtube.com/embed/ejEINaEQieY"
+            title="Buddie Demo"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            frameBorder="0"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              border: "0",
+            }}
+          ></iframe>
+        </div>
+      </div>
+    </div>
   );
 }
 
@@ -44,18 +198,8 @@ export default function Home() {
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
-      <div style={{ marginTop: "2rem", display: "flex", justifyContent: "center" }}>
-        <iframe
-          style={{ borderRadius: '12px' }}  // 👈 设置圆角
-          width="30%"
-          height="400"
-          src="https://www.youtube.com/embed/ejEINaEQieY"
-          title="Buddie Demo"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
+      <HomepageWorkDisplay />
+      <HomepageProductDisplay />
       <main>
         <HomepageFeatures />
       </main>
