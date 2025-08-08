d52b5721ca8730a520ec30c9e75b0dacab68146a
Nanachilil <24212010050@m.fudan.edu.cn>
2025-08-08 16:14:18 +0800

    添加url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,10 +31,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: "https://buddie-official.github.io",
+  url: "https://bud.inc",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/open-source-web/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
